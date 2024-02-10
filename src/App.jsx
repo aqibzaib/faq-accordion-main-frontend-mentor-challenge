40504c3ee45e4f7022c50bfda9e0c6eed26a29d4
Aqib <chaqib4u@gmail.com>
2024-02-11 01:22:38 +0500

    set background image , set hover effect on button and add make page responsive

--- src/App.jsx
+++ src/App.jsx
@@ -6,53 +6,50 @@ import IconStar from "../assets/images/icon-star";
 function App() {
   const [activeIndex, setActiveIndex] = useState(null);
 
-  // const toggleCollapse = (index) => {
-  //   setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
-  // };
   const toggleCollapse = (index) => {
     setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
   };
 
   return (
     <>
-      <div className="bg-[url('../assets/images/background-pattern-desktop.svg')] bg-cover bg-center bg-no-repeat">
-        <div className="border border-white rounded-lg w-[590px] h-fit m-auto px-8 py-4 bg-white">
-          <div>
-            <div className=" flex items-center justify-start gap-4 pt-4 pb-4">
-              <span>
-                <IconStar />
-              </span>
-              <h1 className="text-4xl"> FAQs</h1>
-            </div>
-            {faqData.map((item, index) => {
-              return (
-                <div key={index}>
-                  <button
-                    className={`w-[100%] flex items-center justify-between pt-3 pb-3 ${
-                      activeIndex === index
-                        ? "transition-border duration-300 ease-in-out"
-                        : "border-t-2 border-red-900 transition-border duration-500 ease-in-out"
-                    }`}
-                    onClick={() => toggleCollapse(index)}
-                  >
-                    <span>{item.question}</span>
-                    <span className="pl-5 pr-5 ">
-                      {activeIndex === index ? <IconMinus /> : <IconPlus />}
-                    </span>
-                  </button>
-                  <div
-                    className={`overflow-hidden transition-max-h ${
-                      activeIndex === index
-                        ? "duration-500 ease-in-out max-h-screen"
-                        : "duration-300 ease-in-out max-h-0"
-                    }`}
-                  >
-                    <p className="pt-4 pb-4">{item.answer}Hello</p>
-                  </div>
-                </div>
-              );
-            })}
+      <div className="border border-white rounded-lg min-w-[100%] max-w-[590px] h-fit m-auto px-8 py-4 bg-white shadow-2xl shadow-[hsl(292,16%,49%)]">
+        <div>
+          <div className="flex items-center justify-start gap-4 pt-4 pb-4">
+            <span>
+              <IconStar />
+            </span>
+            <h1 className="text-4xl"> FAQs</h1>
           </div>
+          {faqData.map((item, index) => {
+            return (
+              <div key={index}>
+                <button
+                  className={`w-[100%] flex items-center justify-between pt-3 pb-3 cursor-pointer  font-semibold text-wrap ${
+                    activeIndex === index
+                      ? "transition-border duration-300 ease-in-out"
+                      : "border-t-2 border-red-900 transition-border duration-500 ease-in-out"
+                  }`}
+                  onClick={() => toggleCollapse(index)}
+                >
+                  <span className="hover:text-[hsl(292,16%,49%)]">
+                    {item.question}
+                  </span>
+                  <span className="pl-5 pr-5 ">
+                    {activeIndex === index ? <IconMinus /> : <IconPlus />}
+                  </span>
+                </button>
+                <div
+                  className={`overflow-hidden transition-max-h ${
+                    activeIndex === index
+                      ? "duration-500 ease-in-out max-h-screen"
+                      : "duration-300 ease-in-out max-h-0"
+                  }`}
+                >
+                  <p className="pb-4 bg-white">{item.answer}Hello</p>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
@@ -68,17 +65,17 @@ const faqData = [
   {
     question: "Is Frontend Mentor free?",
     answer:
-      "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
+      "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skills levels.",
   },
   {
     question: "Can I use Frontend Mentor projects in my portfolio?",
     answer:
-      "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
+      "Yes, you can use projects completed on Frontend Mentor in your portfolio, it's an excellent way to showcase your skills to potential employer.",
   },
   {
     question: "How can I get help if I'm stuck on a challenge?",
     answer:
-      "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
+      "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.",
   },
 ];
 
