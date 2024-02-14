a567043ac06d191d4ae880cfcefc4083aa2bbf50
Aqib <chaqib4u@gmail.com>
2024-02-15 02:10:21 +0500

    set padding and width of container

--- src/App.jsx
+++ src/App.jsx
@@ -12,44 +12,47 @@ function App() {
 
   return (
     <>
-      <div className="border border-white rounded-lg min-w-[100%] max-w-[590px] h-fit m-auto px-8 py-4 bg-white drop-shadow-lg shadow-[hsl(292,16%,49%)]">
+      <div className="border border-white rounded-lg min-w-[100%] max-w-[608px] h-fit m-auto px-8 py-4 bg-white drop-shadow-lg shadow-[hsl(292,16%,49%)]">
         <div>
-          <div className="flex items-center justify-start gap-4 pt-4 pb-4">
+          <header className="flex items-center justify-start gap-4 pt-5 pb-5">
             <span>
               <IconStar />
             </span>
             <h1 className="text-4xl"> FAQs</h1>
-          </div>
-          {faqData.map((item, index) => {
-            return (
-              <div key={index}>
-                <button
-                  className={`w-[100%] flex items-center justify-between pt-5 pb-5 cursor-pointer  font-semibold text-wrap ${
-                    activeIndex === index
-                      ? "transition-border duration-300 ease-in-out "
-                      : "border-t-2 border-[#e4e4e4] transition-border duration-500 ease-in-out "
-                  }`}
-                  onClick={() => toggleCollapse(index)}
-                >
-                  <span className="hover:text-[hsl(292,16%,49%)]">
-                    {item.question}
-                  </span>
-                  <span className="pl-5 pr-5 ">
-                    {activeIndex === index ? <IconMinus /> : <IconPlus />}
-                  </span>
-                </button>
-                <div
-                  className={`overflow-hidden transition-max-h ${
-                    activeIndex === index
-                      ? "duration-500 ease-in-out max-h-screen"
-                      : "duration-300 ease-in-out max-h-0"
-                  }`}
-                >
-                  <p className="pb-4 bg-white ">{item.answer}</p>
+          </header>
+          <main>
+            {" "}
+            {faqData.map((item, index) => {
+              return (
+                <div key={index}>
+                  <button
+                    className={`w-[100%] flex items-center justify-between pt-6 pb-6 cursor-pointer  font-semibold text-wrap ${
+                      activeIndex === index
+                        ? "transition-border duration-300 ease-in-out "
+                        : "border-t-2 border-[#e4e4e4] transition-border duration-500 ease-in-out "
+                    }`}
+                    onClick={() => toggleCollapse(index)}
+                  >
+                    <span className="hover:text-[hsl(292,16%,49%)]">
+                      {item.question}
+                    </span>
+                    <span className="pl-5 pr-5 ">
+                      {activeIndex === index ? <IconMinus /> : <IconPlus />}
+                    </span>
+                  </button>
+                  <div
+                    className={`overflow-hidden transition-max-h ${
+                      activeIndex === index
+                        ? "duration-500 ease-in-out max-h-screen"
+                        : "duration-300 ease-in-out max-h-0"
+                    }`}
+                  >
+                    <p className="pb-4 bg-white ">{item.answer}</p>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </main>
         </div>
       </div>
     </>
