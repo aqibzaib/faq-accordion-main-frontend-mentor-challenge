3fcfd3313fbc347e6a860d0475e21915970dfd39
Aqib <chaqib4u@gmail.com>
2024-02-15 02:01:32 +0500

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import IconPlus from "../assets/images/icon-plus";
 import IconStar from "../assets/images/icon-star";
 
 function App() {
-  const [activeIndex, setActiveIndex] = useState(null);
+  const [activeIndex, setActiveIndex] = useState(0);
 
   const toggleCollapse = (index) => {
     setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <>
-      <div className="border border-white rounded-lg min-w-[100%] max-w-[590px] h-fit m-auto px-8 py-4 bg-white shadow-2xl shadow-[hsl(292,16%,49%)]">
+      <div className="border border-white rounded-lg min-w-[100%] max-w-[590px] h-fit m-auto px-8 py-4 bg-white drop-shadow-lg shadow-[hsl(292,16%,49%)]">
         <div>
           <div className="flex items-center justify-start gap-4 pt-4 pb-4">
             <span>
@@ -24,10 +24,10 @@ function App() {
             return (
               <div key={index}>
                 <button
-                  className={`w-[100%] flex items-center justify-between pt-3 pb-3 cursor-pointer  font-semibold text-wrap ${
+                  className={`w-[100%] flex items-center justify-between pt-5 pb-5 cursor-pointer  font-semibold text-wrap ${
                     activeIndex === index
-                      ? "transition-border duration-300 ease-in-out"
-                      : "border-t-2 border-red-900 transition-border duration-500 ease-in-out"
+                      ? "transition-border duration-300 ease-in-out "
+                      : "border-t-2 border-[#e4e4e4] transition-border duration-500 ease-in-out "
                   }`}
                   onClick={() => toggleCollapse(index)}
                 >
@@ -45,7 +45,7 @@ function App() {
                       : "duration-300 ease-in-out max-h-0"
                   }`}
                 >
-                  <p className="pb-4 bg-white">{item.answer}Hello</p>
+                  <p className="pb-4 bg-white ">{item.answer}</p>
                 </div>
               </div>
             );
